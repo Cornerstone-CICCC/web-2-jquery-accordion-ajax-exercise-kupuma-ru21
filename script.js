c915b0b0ad6ebc2f68a0b305335e86fea622ce24
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-07-31 13:29:12 -0700

    make homework done

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-$(function() {
-  // your code here
-})
+$(function () {
+  $(".accordion-header").click(function () {
+    var $content = $(this).next(".accordion-content");
+    $(".accordion-content").not($content).slideUp();
+    $content.slideToggle();
+  });
+
+  $("button:contains('Load To-Do Items')").click(() => {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      success: function (result) {
+        result.todos.forEach(({ todo }) => {
+          $("ul").append(`<li>${todo}</li>`);
+        });
+      },
+    });
+  });
+});
